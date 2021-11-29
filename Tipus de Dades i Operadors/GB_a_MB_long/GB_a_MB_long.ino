cda0339d6b3ce28b2d5234478f52efb3fc90d28b
GerardGonzalez2021 <94805830+GerardGonzalez2021@users.noreply.github.com>
2021-11-29 16:21:50 +0100

    Tipus de dades 2

--- Tipus de Dades i Operadors/GB_a_MB_long/GB_a_MB_long.ino
+++ Tipus de Dades i Operadors/GB_a_MB_long/GB_a_MB_long.ino
@@ -10,7 +10,7 @@
 
 //********************** VARIABLES *************************
 int drive_gb = 100;
-long drive_mb = 0;
+long drive_mb;
 //************************ SETUP ***************************
 void setup() {
   Serial.begin(9600);     // set up Serial library at 9600 bps
@@ -18,7 +18,8 @@ void setup() {
   Serial.print(drive_gb);
   Serial.println(" GB large.");
 
-  drive_mb = 1024 * drive_gb;
+  drive_mb = drive_gb;
+  drive_mb = 1024 * drive_mb;
 
   Serial.print("It can store ");
   Serial.print(drive_mb);
