2d2847d9b629cdb7a291e903b9686dd2df291c13
GerardGonzalez2021 <94805830+GerardGonzalez2021@users.noreply.github.com>
2021-12-10 15:38:24 +0100

    Actualitzacio

---
+++ Estructures de Control de Selecció if & switch/Sensor_Llum/Sensor_Llum.ino
@@ -0,0 +1,42 @@
+/***********************************************************
+**                                                        **
+**                          Títol:                        **
+**                                                        **
+**                                                        **
+**                                                        **
+**   NOM: Gerard Gonzalez Torres             Data:        **
+***********************************************************/
+//*********************** INCLUDE **************************
+
+//********************** VARIABLES *************************
+int sensorReading = 9;
+//************************ SETUP ***************************
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("The day is "); 
+   
+  switch (sensorReading) {
+  case 0:    
+    Serial.println("dark");
+    break;
+  case 1:    
+    Serial.println("dim");
+    break;
+  case 2:    
+    Serial.println("medium");
+    break;
+  case 3:
+    Serial.println("bright");
+    break;
+  default:
+    Serial.println("... I don't know!!!");
+  } 
+}
+
+//************************* LOOP ***************************
+void loop() {
+  
+
+}
+//*********************** FUNCTIONS ************************
